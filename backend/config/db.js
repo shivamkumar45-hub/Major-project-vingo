aef0dc10375c357c27a88798bea0822260dce157
Shivam Kumar <shivam792004@gmail.com>
2026-06-29 21:07:05 +0530

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+const connectDB=async ()=>{
+
+    try{
+       await mongoose.connect(process.env.MONGODB_URL)
+       console.log("Connected to MongoDB")
+
+    }catch(error){
+        console.error("Error connecting to MongoDB:", error)
+
+    }
+}
+export default connectDB;
